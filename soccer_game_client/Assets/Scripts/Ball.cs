540dccc34ba5c98aa68395f055fd66820c4829f7
Majak <majakthecoder@gmail.com>
2018-11-30 00:43:59 -0800

    what next

--- soccer_game_client/Assets/Scripts/Ball.cs
+++ soccer_game_client/Assets/Scripts/Ball.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// Create an IBall interface and do the same steps as for the player.
+
 public class Ball
 {
     public IPlayer Player { get; private set; }

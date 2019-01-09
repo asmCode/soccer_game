8f4f05a73d82a8859ac1ba882ecd170b89d34e13
Majak <majakthecoder@gmail.com>
2019-01-08 23:46:35 -0800

    game mode scene, wip

---
+++ soccer_game_client/Assets/Scripts/Ui/GameModePresenter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameModePresenter : MonoBehaviour
+{
+    public void UIEventStartServer()
+    {
+        Debug.Log("server");
+    }
+
+    public void UIEventFindOnlineMatch()
+    {
+        Debug.Log("online");
+    }
+
+    public void UIEventLocal()
+    {
+        Debug.Log("local");
+    }
+}

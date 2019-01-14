6d9106570e59b265fea13c22976eb5be1011d19f
Majak <majakthecoder@gmail.com>
2019-01-13 22:05:58 -0800

    wip

--- soccer_game_client/Assets/ServerAddressGetter.cs
+++ soccer_game_client/Assets/ServerAddressGetter.cs
@@ -7,5 +7,6 @@ public class ServerAddressGetter
     public INetworkAddress GetServerAddress()
     {
         return new UdpNetworkAddress("192.168.0.110", GameSettings.ServerDefaultPort);
+        //return new UdpNetworkAddress("73.93.128.73", GameSettings.ServerDefaultPort);
     }
 }

01213d49e06b7e9e0c3f4c124324cd10de4484fa
Szymon Majewski <szymon@limbic.com>
2019-10-10 18:05:39 -0700

    wip, build break

--- soccer_game_client/Assets/Scripts/Physics/Collisions/Collider.cs
+++ soccer_game_client/Assets/Scripts/Physics/Collisions/Collider.cs
@@ -1,18 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace ssg
 {
     public abstract class Collider
     {
-        public event CollisionDelegate Collision;
+        public event CollisionDelegate CollisionEnter;
+        public event CollisionDelegate CollisionLeave;
         public ITransformable Trans;
+        public HashSet<Collider> CurrentColliders = new HashSet<Collider>();
 
         public abstract ColliderId ColliderId { get; }
 
-        public void NotifyCollision(Collider otherCollider)
+        public void NotifyCollisionEnter(Collider otherCollider)
         {
-            if (Collision != null)
-                Collision(otherCollider);
+            CurrentColliders.Add(otherCollider);
+
+            if (CollisionEnter != null)
+                CollisionEnter(otherCollider);
+        }
+
+        public void NotifyCollisionLeave(Collider otherCollider)
+        {
+            CurrentColliders.Remove(otherCollider);
+
+            if (CollisionLeave != null)
+                CollisionLeave(otherCollider);
         }
     }
 }
